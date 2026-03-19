0b83594a1757b4cfcd9764e5e6c967172a41bc26
Meduz1607 <0xmeduz@gmail.com>
2026-03-19 10:44:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import os
+from telegram import Bot
+
+# Environment variable’dan token olish
+TOKEN = os.getenv("BOT_TOKEN")
+
+# Bot yaratish
+bot = Bot(token=TOKEN)
+
+# Ishlayapti degan xabar
 print("Bot ishlayapti 🚀")
